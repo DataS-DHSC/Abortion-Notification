595fcc5262ce75f186db8756ae11687ef7611249
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-05-16 17:23:56 +0100

    10 character validation working

--- DHSC.ANS.Service/app/views/questions/patient-details/do-you-know-the-patients-hospital-or-clinic-number.js
+++ DHSC.ANS.Service/app/views/questions/patient-details/do-you-know-the-patients-hospital-or-clinic-number.js
@@ -1,4 +1,5 @@
 function resolve(sessionData, formData) {
+    console.log("resolve");
     const answer = sessionData?.patientInformation?.hospitalNumber?.known;
     if (answer === 'yes') return 'what-is-the-patients-hospital-number';
     if (answer === 'no') return 'do-you-know-the-place-number';
@@ -8,6 +9,8 @@ function resolve(sessionData, formData) {
 function validate(sessionData, formData) {
     const errors = [];
 
+    console.log("validate");
+    
     const value = formData?.patientInformation?.hospitalNumber?.known;
     if (!value) {
         errors.push({
@@ -18,11 +21,19 @@ function validate(sessionData, formData) {
 
     // If "yes" is selected, validate the input field
     if (value === 'yes') {
+
         const number = formData?.patientInformation?.hospitalNumber?.number;
+
         if (!number || number.trim() === '') {
             errors.push({
                 field: 'patientInformation.hospitalNumber.number',
                 message: 'Enter the patient’s hospital or clinic number'
+            });
+        } else if (number.length > 10) {
+            console.log("10 chars")
+            errors.push({
+                field: 'patientInformation.hospitalNumber.number',
+                message: 'The hospital or clinic number must be 10 characters or fewer'
             });
         }
     }
