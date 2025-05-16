e4d0411ee29e416e41bd8320cbd2c0fab292c106
Zach Hodges <zach.hodges@hippodigital.co.uk>
2025-05-16 16:04:16 +0100

    wip - new patient details journey

---
+++ DHSC.ANS.Service/app/views/questions/patient-details/do-you-know-the-patients-hospital-or-clinic-number.js
@@ -0,0 +1,36 @@
+function resolve(sessionData, formData) {
+    const answer = sessionData?.patientInformation?.hospitalNumber?.known;
+    if (answer === 'yes') return 'what-is-the-patients-hospital-number';
+    if (answer === 'no') return 'do-you-know-the-place-number';
+    return null;
+}
+
+function validate(sessionData, formData) {
+    const errors = [];
+
+    const value = formData?.patientInformation?.hospitalNumber?.known;
+    if (!value) {
+        errors.push({
+            field: 'patientInformation.hospitalNumber.known',
+            message: 'Select yes if you know the patient’s hospital or clinic number'
+        });
+    }
+
+    // If "yes" is selected, validate the input field
+    if (value === 'yes') {
+        const number = formData?.patientInformation?.hospitalNumber?.number;
+        if (!number || number.trim() === '') {
+            errors.push({
+                field: 'patientInformation.hospitalNumber.number',
+                message: 'Enter the patient’s hospital or clinic number'
+            });
+        }
+    }
+
+    return errors;
+}
+
+module.exports = {
+    resolve,
+    validate
+};
