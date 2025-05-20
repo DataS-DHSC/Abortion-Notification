754cd69214880173907d04f4e1a25b0248404c47
spar-hippo <simon@mmaraboustork.co.uk>
2025-05-20 16:31:24 +0100

    update

--- DHSC.ANS.Service/app/routes.js
+++ DHSC.ANS.Service/app/routes.js
@@ -4,6 +4,7 @@ const router = govukPrototypeKit.requests.setupRouter();
 // ──────────────────────────────────────────────────────────────
 // Mount extra route-files here (one line per file)
 // ──────────────────────────────────────────────────────────────
+router.use('/', require('./routes/default'));      // --> app/routes/forms.js
 router.use('/', require('./routes/forms'));      // --> app/routes/forms.js
 router.use('/', require('./routes/questions'));  // --> app/routes/questions.js
 
