bdae835fbc0f82022638b18ca7e34e0254bdf373
spar-hippo <simon@mmaraboustork.co.uk>
2025-05-21 11:05:22 +0100

    introduce session

--- DHSC.ANS.Service/app/assets/javascripts/application.js
+++ DHSC.ANS.Service/app/assets/javascripts/application.js
@@ -3,8 +3,8 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-import { initAll } from '@ministryofjustice/frontend/moj/all.js';
-initAll();
+// import { initAll } from '@ministryofjustice/frontend/moj/all.js';
+// initAll();
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
--- DHSC.ANS.Service/app/routes.js
+++ DHSC.ANS.Service/app/routes.js
@@ -1,11 +1,19 @@
 const govukPrototypeKit = require('govuk-prototype-kit');
 const router = govukPrototypeKit.requests.setupRouter();
 
+const path    = require('path');
+const express = require('express');
+
 // ──────────────────────────────────────────────────────────────
 // Mount extra route-files here (one line per file)
 // ──────────────────────────────────────────────────────────────
 router.use('/', require('./routes/default'));      // --> app/routes/forms.js
 router.use('/', require('./routes/forms'));      // --> app/routes/forms.js
 router.use('/', require('./routes/questions'));  // --> app/routes/questions.js
+
+router.use(
+  '/assets',
+  express.static(path.join(__dirname, 'assets'))
+);
 
 module.exports = router;
